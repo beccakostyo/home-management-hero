92e94ad36cf00c3a1fdd215079bfea9dabf84b35
beccakostyo <beccalynharris@gmail.com>
2018-07-10 15:29:09 -0600

    more pages and stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import SigninNav from "./components/SigninNav/SigninNav";
 import CopyRightFooter from './components/CopyrightFooter/CopyrightFooter';
+import Home from "./pages/Home/Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SigninNav />
+        <Home />
         <CopyRightFooter/>
       </div>
     );
--- client/src/components/SignupNav/SignupNav.js
+++ client/src/components/SignupNav/SignupNav.js
@@ -3,14 +3,16 @@ import { Navbar, NavItem, Row, Col } from 'react-materialize';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
 
-import './SigninNav.css';
+import './SignupNav.css';
 
 import SigninButton from '../SigninButton/SigninButton';
+import SignupButton from '../SignupButton/SignupButton';
 
 const SigninNav = () => (
   <div class="container">
     <Navbar className="transparent-nav">
       <NavItem className="left-float"><FontAwesomeIcon icon={faHome} size='3x' /></NavItem>
+      <NavItem className="right-float signin-btn"><SignupButton /></NavItem>
       <NavItem className="right-float signin-btn"><SigninButton /></NavItem>
       <NavItem className="right-float">Contact</NavItem>
       <NavItem className="right-float">Help</NavItem>
