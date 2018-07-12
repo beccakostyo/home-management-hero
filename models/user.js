8c485e4a58c4781d5242a7001df845d59cf7af67
beccakostyo <beccalynharris@gmail.com>
2018-07-12 00:12:34 -0600

    implement passport local strategy, get users saving to backend with hashed passwords

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); 
 const Schema = mongoose.Schema;
 
 // Define userSchema
 const userSchema = new Schema({
-
 	username: { 
     type: String, 
     required: true 
@@ -14,6 +14,25 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods = {
+  checkPassword: function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password)
+  },
+  hashPassword: plainTextPassword => {
+    return bcrypt.hashSync(plainTextPassword, 10)
+  }
+};
+
+userSchema.pre('save', function (next) {
+  if (!this.password) {
+    console.log('models/user.js =====NO PASSWORD PROVIDED=====');
+    next()
+  } else {
+    console.log('models/user.js hashPassword in pre save');
+    this.password = this.hashPassword(this.password);
+    next();
+  }
+});
 
 const User = mongoose.model('User', userSchema)
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const passport = require('./passport');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +12,19 @@ const PORT = process.env.PORT || 3001;
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Sessions 
+app.use(
+  session({
+    secret: 'staring-bobcat', // random string to make sure hash that's generated is secure //
+    resave: false, //required
+    saveUninitialized: false //required
+  })
+);
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) //calls serializeUser and deserializeUser
 
 // Serve up static assets (usually on heroku) //
 if (process.env.NODE_ENV === 'production') {
