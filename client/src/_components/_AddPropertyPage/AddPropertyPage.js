9d3f0544fc7ebbc68fc6be6e584751356d5e06da
beccakostyo <beccalynharris@gmail.com>
2018-07-23 20:55:41 -0600

    little tiny cleanup

--- client/src/_components/_AddPropertyPage/AddPropertyPage.js
+++ client/src/_components/_AddPropertyPage/AddPropertyPage.js
@@ -7,19 +7,19 @@ const AddPropertyPage = () => (
   <div className='app'>
     <SignupNav />
     <div className="container">
-    <Card>
-      <div className='center-align' id="card-content">
-        <h1 className='add-prop-header'>Add a new property</h1>
-        <p className='flow-text'>
-          Start with the basics...enter in a name for your property, the address, and the phone number. You may also upload a picture if you choose!
-          <br/><br/>
-          Then, click the "Important Contacts" label and enter in more info there. 
+      <Card>
+        <div className='center-align' id="card-content">
+          <h1 className='add-prop-header'>Add a new property</h1>
+          <p className='flow-text'>
+            Start with the basics...enter in a name for your property, the address, and the phone number. You may also upload a picture if you choose!
+          <br /><br />
+            Then, click the "Important Contacts" label and enter in more info there.
         </p>
-        <br/>
-        <p><em>* Denotes required field</em></p>
+          <br />
+          <p><em>* Denotes required field</em></p>
         </div>
-        <AddPropertyForm/>
-    </Card>
+        <AddPropertyForm />
+      </Card>
     </div>
   </div>
 )
